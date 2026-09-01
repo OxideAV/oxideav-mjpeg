322a65a5446b0ecee4c2714fadc191c13c73657b
Mark Karpeles <magicaltux@gmail.com>
2026-09-01 10:25:14 +0900

    docs: hide internal pub surface from rustdoc/semver (fleet rule 2026-09-01)

--- src/jpeg/mod.rs
+++ src/jpeg/mod.rs
@@ -1,11 +1,19 @@
 //! Baseline JPEG bitstream primitives.
 
+#[doc(hidden)]
 pub mod arith;
+#[doc(hidden)]
 pub mod color;
+#[doc(hidden)]
 pub mod dct;
+#[doc(hidden)]
 pub mod huffman;
 pub mod inspect;
+#[doc(hidden)]
 pub mod markers;
+#[doc(hidden)]
 pub mod parser;
+#[doc(hidden)]
 pub mod quant;
+#[doc(hidden)]
 pub mod zigzag;
